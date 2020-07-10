e8581357c4b605617bbb1bc0f4221c3933b4e48c
Pavel Ovechkin <pas7273@gmail.com>
2020-07-11 01:28:06 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+<h1 align="center">Проект "Россия"</h1>
+<p align="center">
+  <img src="https://img.shields.io/badge/made%20by-opv1-blue.svg">
+</p>
+
+## Описание
+
+Верстка адаптивной страницы с макета (desktop first)
+
+## [Увидеть проект](https://opv1.github.io/yp-course-2/)
---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+const { src, dest, series, watch } = require('gulp');
+const deploy = require('gulp-deploy-git');
+const include = require('gulp-file-include');
+const sync = require('browser-sync').create();
+const del = require('del');
+const concat = require('gulp-concat');
+const htmlmin = require('gulp-htmlmin');
+const sass = require('gulp-sass');
+const csso = require('gulp-csso');
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const tildeImporter = require('node-sass-tilde-importer');
+const fontmin = require('gulp-fontmin');
+
+function htmlRu() {
+  return src('./src/index.html')
+    .pipe(
+      include({
+        prefix: '@@',
+      })
+    )
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+      })
+    )
+    .pipe(dest('./dist'));
+}
+
+function htmlEn() {
+  return src('./src/en/index.html')
+    .pipe(
+      include({
+        prefix: '@@',
+      })
+    )
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+      })
+    )
+    .pipe(dest('./dist/en'));
+}
+
+function scss() {
+  return src('./src/styles/index.scss')
+    .pipe(
+      sass({
+        importer: tildeImporter,
+      })
+    )
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ['last 2 versions'],
+      })
+    )
+    .pipe(csso())
+    .pipe(concat('index.css'))
+    .pipe(dest('./dist/styles'));
+}
+
+function images() {
+  return src('./src/images/*').pipe(imagemin()).pipe(dest('./dist/images'));
+}
+
+function fonts() {
+  return src('./src/fonts/*').pipe(fontmin()).pipe(dest('./dist/fonts'));
+}
+
+function clear() {
+  return del('./dist');
+}
+
+function server() {
+  sync.init({
+    server: './dist',
+  });
+
+  watch('./src/parts/**.html', series(htmlRu)).on('change', sync.reload);
+  watch('./src/en/parts/**.html', series(htmlEn)).on('change', sync.reload);
+  watch('./src/styles/**.scss', series(scss)).on('change', sync.reload);
+}
+
+function publish() {
+  return src('./dist/**/*', { read: false }).pipe(
+    deploy({
+      repository: 'https://github.com/opv1/yp-course-2.git',
+      branches: ['gh-pages'],
+    })
+  );
+}
+
+exports.build = series(clear, fonts, images, htmlRu, htmlEn, scss);
+exports.server = series(clear, fonts, images, htmlRu, htmlEn, scss, server);
+exports.deploy = series(publish);
